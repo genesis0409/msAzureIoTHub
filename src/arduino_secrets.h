6ac5122a8f12d96d24af5aaac761f715169006ed
Daon_Sori <smcdidcksthf@naver.com>
2024-01-31 17:55:37 +0900

    first commit

---
+++ src/arduino_secrets.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+
+using namespace std;
+
+// Fill in your WiFi networks info
+#define SECRET_WIFI_SSID "dinfo"
+#define SECRET_WIFI_PASS "daon7521"
+
+// Fill in the hostname of your Azure IoT Hub broker
+#define SECRET_BROKER "daonHubGroup.azure-devices.net"
+
+// Fill in the device id
+#define SECRET_DEVICE_ID "esp32_Seonsor_temp"
+
+// Fill in the device password: SAS token; 10000hours... === 416days
+#define SECRET_DEVICE_PASSWORD "SharedAccessSignature sr = daonHubGroup.azure - devices.net % 2Fdevices % 2Fesp32_Seonsor_temp &sig = io2otXaQAlujBnytNDjHdgih0TlIsjeRBWEfztE3Lok % 3D &se = 1742683722"
